c1065549a586cbfc3f08ff29338c17f225831460
Jackson147 <jaswahhihi@gmail.com>
2025-10-05 10:16:31 +0100

    feat: added framework to upload a photo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import type { Metadata } from "next";
+import Link from "next/link"; // <-- Added Link for navigation
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
@@ -25,7 +26,23 @@ export default function RootLayout({
 	return (
 		<html lang="en" className={`${geist.variable}`}>
 			<body>
-				<TRPCReactProvider>{children}</TRPCReactProvider>
+				<TRPCReactProvider>
+					{/* Simple Fixed Navigation Bar */}
+					<nav className="sticky top-0 z-50 bg-[#2e026d] p-4 shadow-lg flex justify-center">
+						<div className="flex space-x-6 text-xl font-semibold">
+							<Link href="/" className="text-white hover:text-[hsl(280,100%,70%)] transition duration-150">
+								Home
+							</Link>
+							<Link href="/upload" className="text-white hover:text-[hsl(280,100%,70%)] transition duration-150">
+								Upload Photo
+							</Link>
+						</div>
+					</nav>
+					
+					{/* Content */}
+					{children}
+					
+				</TRPCReactProvider>
 			</body>
 		</html>
 	);

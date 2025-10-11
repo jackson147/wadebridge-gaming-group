e3b5fb8e3fbee6c3cafd8e83453a4c351576ad78
Jackson147 <jaswahhihi@gmail.com>
2025-10-11 17:21:00 +0100

    fix: tidied up

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
-import { uploadRouter } from "./routers/upload";
 
 /**
  * This is the primary router for your server.
@@ -9,7 +8,6 @@ import { uploadRouter } from "./routers/upload";
  */
 export const appRouter = createTRPCRouter({
 	post: postRouter,
-	upload: uploadRouter
 });
 
 // export type definition of API
